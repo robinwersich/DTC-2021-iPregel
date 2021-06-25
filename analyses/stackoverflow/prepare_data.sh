1409486579d9728d574863e354e1b2b26d0dda31
Robin Wersich <robin.wersich@student.hpi.de>
2021-06-25 12:41:49 +0200

    add stackoverflow preparation skript

--- analyses/stackoverflow/prepare_data.sh
+++ analyses/stackoverflow/prepare_data.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+# navigate to root directory
+cd "$(dirname "$0")"
+
+if [ -d "data" ]; then
+    echo "Data already present."
+    exit
+fi
+
+( # execute in subshell to catch errors and clean up afterwards
+    set -e
+    
+    mkdir data && cd data
+    echo "Downloading..."
+    curl https://snap.stanford.edu/data/sx-stackoverflow.txt.gz --output stackoverflow-original.txt.gz
+    echo "Extracting..."
+    gzip -d stackoverflow-original.txt.gz
+    echo "Removing timestamps, reversing edges and removing duplicates..."
+    echo "This is a huge graph, so it may take quite a while - relax."
+    python ../reverse_remove_timestamp.py stackoverflow-original.txt | sort | uniq > stackoverflow.txt
+    rm stackoverflow-original.txt
+)
+if [ $? -ne 0 ]; then
+    echo "Data preparation failled. Exiting."
+    rm -rf "data"
+    exit 1
+fi
