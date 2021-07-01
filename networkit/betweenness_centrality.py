adb75e7f05313d4ba573804934a04ee102df62b8
phoeinx <hradilak@uni-potsdam.de>
2021-07-01 14:38:09 +0200

    Add numberOfThread parameter to networkit scripts

--- networkit/betweenness_centrality.py
+++ networkit/betweenness_centrality.py
@@ -4,8 +4,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("input", help="(Relative Path to) graph in edgelist format with space seperator (e.g. SNAP) to run algorithm on.")
+parser.add_argument("input", help="(Relative Path to) graph in edgelist format (e.g. SNAP) to run algorithm on. Default seperator is tab.")
 parser.add_argument("output", help="Relative path to name of outputfile.")
+
+parser.add_argument("--numThreads", type=int, dest='numThreads', default=1, help="Optionally set maximum number of available threads. Default is 1.")
 
 parser.add_argument("--directed", dest='directed', action='store_true', help="Optionally specify directedness of graph. Default is directed.")
 parser.add_argument("--undirected", dest='directed', action='store_false', help="Optionally specify directedness of graph. Default is directed.")
@@ -17,6 +19,8 @@ parser.add_argument("--normalized", dest='normalized', action='store_true', help
 parser.set_defaults(directed=True, normalized=False)
 
 args = parser.parse_args()
+
+nk.setNumberOfThreads(args.numThreads)
 
 try:
     G = nk.graphio.EdgeListReader(separator=args.seperator, firstNode=args.firstnode, directed=args.directed).read(args.input)
