0528583eb167f28425f4643cd8c2dbfeda62ee70
Robin Wersich <robin.wersich@student.hpi.de>
2021-07-01 14:15:39 +0200

    add venv fallback

--- utility/setup.sh
+++ utility/setup.sh
@@ -63,7 +63,7 @@ else
         trap "echo && exit 1" SIGINT
 
         echo "Creating virtual python environment in $(pwd)"
-        python3 -m venv .venv
+        python -m venv .venv || virtualenv .venv 1> /dev/null
         source .venv/bin/activate
         echo "Installing python dependencies..."
         pip install --upgrade pip 1> /dev/null
