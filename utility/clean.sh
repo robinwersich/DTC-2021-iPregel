ec0e2a9bcd726e8e5f73598d7bcee9b947d0f9c8
Robin Wersich <robin.wersich@student.hpi.de>
2021-08-12 14:58:13 +0200

    improve clean utility

--- utility/clean.sh
+++ utility/clean.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
+LIGRA_DIR="ligra"
+
 cd $(dirname "$0")/..
 
 # help text
 if [ "$1" = "--help" ] || [ "$1" = "-h" ] || [ "$1" = "" ]; then
-    echo "usage: $0 [--bin] [--benchmarks] [--results] [--data-prepared] [--data-downloads] [--data] [--all]"
+    echo "usage: $0 OPTION"
+    echo "possible OPTIONs:"
+    echo "--bin: remove iPregel binaries"
+    echo "--benchmarks: remove benchmark results"
+    echo "--results: remove analysis results"
+    echo "--data-prepared: remove preprocessed data, but keep downloads"
+    echo "--data-downloads: remove original data downloads, but keep preprocessed data"
+    echo "--data: remove all graph data"
+    echo "--uninstall: remove all previously mentioned data and uninstall ligra and networkit"
     exit
 fi
 
-if [ "$1" = "--bin" ] || [ "$1" = "--all" ]; then
-    make clean -C "iPregel"
+if [ "$1" = "--bin" ] || [ "$1" = "--uninstall" ]; then
+    make clean -C "iPregel" > /dev/null
 fi
 
-if [ "$1" = "--benchmarks" ] || [ "$1" = "--all" ]; then
+if [ "$1" = "--benchmarks" ] || [ "$1" = "--uninstall" ]; then
     rm -rf "benchmark_results"
 fi
 
-if [ "$1" = "--results" ] || [ "$1" = "--all" ]; then
+if [ "$1" = "--results" ] || [ "$1" = "--uninstall" ]; then
     rm -rf analyses/*/results
 fi
 
-if [ "$1" = "--data-prepared" ] || [ "$1" = "--data" ] || [ "$1" = "--all" ]; then
+if [ "$1" = "--data-prepared" ] || [ "$1" = "--data" ] || [ "$1" = "--uninstall" ]; then
     rm -rf analyses/*/data_prepared
 fi
 
-if [ "$1" = "--data-downloads" ] || [ "$1" = "--data" ] || [ "$1" = "--all" ]; then
+if [ "$1" = "--data-downloads" ] || [ "$1" = "--data" ] || [ "$1" = "--uninstall" ]; then
     rm -rf analyses/*/data_original
+fi
+
+if [ "$1" = "--uninstall" ]; then
+    rm -rf .venv
+    rm -rf "../$LIGRA_DIR"
 fi
